e8c68e77f9048a181a3a2937286fe3de43ac4f35
eikyuu <v.duguet@laposte.net>
2020-10-20 10:47:08 +0200

    feature filmDetail start

--- Services/TMDApi.js
+++ Services/TMDApi.js
@@ -12,15 +12,12 @@ export function getFilmsFromApiWithSearchedText(text, page) {
   return axios.get(url).then((response) => response.data);
 }
 
-// export function getFilmsFromApiWithSearchedText(text, page) {
-//   const url =
-//     "https://api.themoviedb.org/3/search/movie?api_key=" +
-//     API_TOKEN +
-//     "&language=fr&query=" +
-//     text +
-//     "&page=" +
-//     page;
-//   return fetch(url)
-//     .then((response) => response.json())
-//     .catch((error) => console.error(error));
-// }
+export function getFilmDetailFromApi(id) {
+  const url =
+    "https://api.themoviedb.org/3/movie/" +
+    id +
+    "?api_key=" +
+    API_TOKEN +
+    "&language=fr";
+  return axios.get(url).then((response) => response.data);
+}
