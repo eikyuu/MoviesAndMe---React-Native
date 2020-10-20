b6a7bb63f900aeac7e95612bc30c80b65fae9b4c
eikyuu <v.duguet@laposte.net>
2020-10-20 13:01:06 +0200

    feature filmDetail advanced

--- components/FilmDetail.js
+++ components/FilmDetail.js
@@ -1,5 +1,7 @@
+import moment from "moment";
+import numeral from "numeral";
 import React, { useEffect, useState } from "react";
-import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
+import { ActivityIndicator, Image, StyleSheet, Text, View } from "react-native";
 import { ScrollView } from "react-native-gesture-handler";
 import { getFilmDetailFromApi } from "../Services/TMDApi";
 
@@ -38,7 +40,44 @@ const FilmDetail = ({ route }) => {
     if (!isLoading) {
       return (
         <ScrollView style={styles.scrollview_container}>
-          <Text>{film.title}</Text>
+          <Image
+            style={styles.image}
+            source={{
+              uri: "https://image.tmdb.org/t/p/original" + film.backdrop_path,
+            }}
+          />
+          <Text style={styles.title}>{film.title}</Text>
+          <Text style={styles.overview}>{film.overview}</Text>
+          <Text style={styles.title_text}>{film.title}</Text>
+          <Text style={styles.description_text}>{film.overview}</Text>
+          <Text style={styles.default_text}>
+            Sorti le {moment(new Date(film.release_date)).format("DD/MM/YYYY")}
+          </Text>
+          <Text style={styles.default_text}>
+            Note : {film.vote_average} / 10
+          </Text>
+          <Text style={styles.default_text}>
+            Nombre de votes : {film.vote_count}
+          </Text>
+          <Text style={styles.default_text}>
+            Budget : {numeral(film.budget).format("0,0[.]00 $")}
+          </Text>
+          <Text style={styles.default_text}>
+            Genre(s) :{" "}
+            {film.genres
+              .map(function (genre) {
+                return genre.name;
+              })
+              .join(" / ")}
+          </Text>
+          <Text style={styles.default_text}>
+            Companie(s) :{" "}
+            {film.production_companies
+              .map(function (company) {
+                return company.name;
+              })
+              .join(" / ")}
+          </Text>
         </ScrollView>
       );
     }
@@ -67,6 +106,28 @@ const styles = StyleSheet.create({
   },
   scrollview_container: {
     flex: 1,
+  },
+  image: {
+    height: 169,
+    margin: 5,
+  },
+  title: {
+    textAlign: "center",
+    fontWeight: "bold",
+    fontSize: 35,
+    flex: 1,
+    flexWrap: "wrap",
+    marginLeft: 5,
+    marginRight: 5,
+    marginTop: 10,
+    marginBottom: 10,
+    color: "#000000",
+  },
+  overview: {
+    fontStyle: "italic",
+    color: "#666666",
+    margin: 5,
+    marginBottom: 15,
   },
 });
 
