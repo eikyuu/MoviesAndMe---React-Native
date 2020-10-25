1a4ab52fe67b38e2acdd6ab3e73c7d15026cadf3
eikyuu <v.duguet@laposte.net>
2020-10-25 18:02:16 +0100

    add components favorites

---
+++ components/Favorites.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Text } from "react-native";
+
+const Favorites = () => {
+  return <Text>Mes favoris</Text>;
+};
+
+const styles = StyleSheet.create({});
+export default Favorites;
