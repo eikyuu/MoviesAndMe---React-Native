44a3f597341414ad33ce14c3e459bbe65c13b99f
eikyuu <v.duguet@laposte.net>
2020-10-25 19:09:16 +0100

    resolve tabnavigation

--- components/Favorites.js
+++ components/Favorites.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text } from "react-native";
+import { StyleSheet, Text } from "react-native";
 
 const Favorites = () => {
   return <Text>Mes favoris</Text>;
--- components/navigation/Navigation.js
+++ components/navigation/Navigation.js
@@ -2,16 +2,27 @@ import { createStackNavigator } from "@react-navigation/stack";
 import Search from "../Search";
 import React from "react";
 import FilmDetail from "../FilmDetail";
+import Favorites from "../Favorites";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
-function Navigation() {
+
+const StackingSearch = () => {
   return (
     <Stack.Navigator>
       <Stack.Screen name="Rechercher" component={Search} />
       <Stack.Screen name="Film Detail" component={FilmDetail} />
     </Stack.Navigator>
   );
-}
+};
+
+const Navigation = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Rechercher" component={StackingSearch} />
+      <Tab.Screen name="Favoris" component={Favorites} />
+    </Tab.Navigator>
+  );
+};
 export default Navigation;
